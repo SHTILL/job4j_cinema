39fdb9aacc8c434416f8622c020e32507e377e50
vasiliy.koryakin <vasiliy.koryakin@gs-labs.ru>
2021-04-05 14:21:10 +0300

    fix singleton

--- src/main/java/ru/job4j/store/PsqlStore.java
+++ src/main/java/ru/job4j/store/PsqlStore.java
@@ -35,13 +35,13 @@ public class PsqlStore implements Store {
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
-        pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
-        pool.setUrl(cfg.getProperty("jdbc.url"));
-        pool.setUsername(cfg.getProperty("jdbc.username"));
-        pool.setPassword(cfg.getProperty("jdbc.password"));
-        pool.setMinIdle(5);
-        pool.setMaxIdle(10);
-        pool.setMaxOpenPreparedStatements(100);
+        this.pool.setDriverClassName(cfg.getProperty("jdbc.driver"));
+        this.pool.setUrl(cfg.getProperty("jdbc.url"));
+        this.pool.setUsername(cfg.getProperty("jdbc.username"));
+        this.pool.setPassword(cfg.getProperty("jdbc.password"));
+        this.pool.setMinIdle(5);
+        this.pool.setMaxIdle(10);
+        this.pool.setMaxOpenPreparedStatements(100);
     }
 
     public static PsqlStore getInstance() {
